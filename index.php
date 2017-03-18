96497a40533ba464944193bb5da2863ebb1d499f
pahul <paul.bouyssounouse@gmail.com>
2017-03-18 14:19:33 +0100

    Bootstap et page d'accueil

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="libs/bootstrap/favicon.ico">
+
+    <title>Dashboard Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="libs/bootstrap/docs/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="libs/bootstrap/docs/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="libs/bootstrap/docs/examples/dashboard/dashboard.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="libs/bootstrap/docs/assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Super soir&eacute;e ;)</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Ajouter un nouvelle vid&eacute;o</a></li>
+            <li><a href="#">Ajouter une actrice</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <input type="text" class="form-control" placeholder="Search...">
+          </form>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">Accueil <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Vid&eacute;os vues</a></li>
+            <li><a href="#">Vid&eacute;os &agrave; voir</a></li>
+            <li><a href="#">Vid&eacute;os &agrave revoir ;)</a></li>
+          </ul>
+          
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">Accueil</h1>
+
+          <div class="row placeholders">
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img src="http://i.ebayimg.com/images/i/311596157953-0-1/s-l1000.jpg" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Romi Rain</h4>
+              <span class="text-muted">The best of the best</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img src="http://img.crocostars.com/katie-kox/doctor-adventures/mouth-watering-katie-kox-hot-vixen/big.jpg" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Katie Kox</h4>
+              <span class="text-muted">La gourmande</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img src="https://pbs.twimg.com/media/Cyhr3HrWEAAFc0B.jpg" width="200" height="400" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Sophie Dee</h4>
+              <span class="text-muted">serial croqueuse</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img src="http://www.nexxx.com/media/misc/model742.jpg" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Peta Jensen</h4>
+              <span class="text-muted">sage ... en apparence </span>
+            </div>
+          </div>
+
+          
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="libs/bootstrap/docs/assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="libs/bootstrap/docs/dist/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="libs/bootstrap/docs/assets/js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="libs/bootstrap/docs/assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
